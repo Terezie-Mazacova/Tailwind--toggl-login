aa0a5e275682d4af6d22474617be97735aa06ec5
Terezie Mazáčová <terezkamazacova@post.cz>
2024-08-15 21:08:24 +0200

    zmena barvy u inputu, ikona u hesla a par mensich zmen

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
